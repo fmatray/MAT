ade9be905de7952eb81c52736e97d69d27a4ab52
frederic.matray@me.com <pi@atlas>
2014-08-06 14:54:30 +0200

    modified:   schedule.py

--- daemon/schedule.py
+++ daemon/schedule.py
@@ -16,12 +16,12 @@ class Schedule:
       DataBase.close()
       sys.exit()
 
-  def GetAlarms(self):
-    AlarmList = []
+  def Alarms(self):
     IDList = []
+    LocalTime = datetime.datetime.now()
     self.Cursor.execute("SELECT * FROM alarm WHERE isactive=true")
     R = ["0000","00","00","00","00"]
-    for Row in self.Cursor.fetchall() :
+    for Row in self.Cursor.fetchall():
       ID = Row[0]
       for i in range(0, 5):
         if Row[i + 1] == "*":
@@ -29,16 +29,21 @@ class Schedule:
           R[i] = str(time.localtime()[i])
         else:
          R[i] = str(Row[i + 1])
-
+# ALARM on date and time 
       Alarm = datetime.datetime.strptime(R[0] + "." + R[1] + "." + R[2] + " " + R[3] + ":" + R[4], "%Y.%m.%d %H:%M")
-      if Alarm >= datetime.datetime.now(): 
-        AlarmList.append(Alarm) 
-      elif ID <> None:
+      if Alarm < LocalTime and LocalTime < Alarm + datetime.timedelta(0, 10):
+        self.SerialData += "alarm\n"
+# ALARM on week day
+      for i in range(0, 6):
+        if Row[i + 6] == 1:
+          ID = None
+          if i == time.localtime()[6] and Alarm < LocalTime and LocalTime < Alarm + datetime.timedelta(0, 10):
+            self.SerialData += "alarm\n"
+# REMOVE old alarm
+      if ID <> None:
         IDList.append(Row[0])
-
     for ID in IDList:
       self.Cursor.execute("UPDATE alarm SET isactive=0 WHERE ID =" + str(ID))
-    return AlarmList
   
   def GetSerialData(self):
     return self.SerialData
@@ -47,13 +52,7 @@ class Schedule:
     self.SerialData = ""
 
   def Schedule(self):
-    AlarmList = self.GetAlarms()
-    LocalTime = datetime.datetime.now()
-    for Alarm in AlarmList:
-      if Alarm < LocalTime and LocalTime < Alarm + datetime.timedelta(0, 10):
-        print "ALARM"
-        self.SerialData = "alarm\n"
-        AlarmList.remove(Alarm)
-    t = Timer(2, self.Schedule)
+    self.Alarms()
+    t = Timer(10, self.Schedule)
     t.start()
 
