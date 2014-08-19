925e128814c1289daab4b4de2c3e923c4f66c377
fmatray <frederic.matray@me.com>
2014-08-19 22:23:48 +0200

    new file:   .gitignore 	new file:   web/cgi-bin/web.py 	new file:   web/css/style.css 	new file:   web/foot.html 	new file:   web/head.html 	new file:   web/index.html 	new file:   web/navbar.html

---
+++ web/cgi-bin/web.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+a = 0
+
+print "Content-type:text/html\r\n\r\n"
+print '<html>'
+print '<head>'
+print '<title>Hello Word - First CGI Program</title>'
+print '</head>'
+print '<body>'
+print '<h2>Hello Word! This is my first CGI program</h2>'
+if a == 0:
+	print 'Le monde est merveilleux'
+	print 'ici aussi'
+print '</body>'
+print '</html>'
